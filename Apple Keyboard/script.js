8f4cbdec9d85d11f61ba5b54bc64be572a677f9e
Anshuman the G.O.A.T. <78291316+anshumanthegoat@users.noreply.github.com>
2025-02-22 17:24:44 +0530

    Delete Apple Keyboard directory

--- Apple Keyboard/script.js
+++
@@ -1,8 +0,0 @@
-// caps lock key
-document.addEventListener("DOMContentLoaded", function() {
-	const capsLock = document.getElementById("caps-lock");
-	capsLock?.addEventListener("click", function() {
-		const pressed = this.ariaPressed === "true";
-		this.setAttribute("aria-pressed",!pressed);
-	});
-});
